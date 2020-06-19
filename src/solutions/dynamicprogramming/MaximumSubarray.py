875ffb72f670f927195a795746d2b4b89feac23f
Nigesh Shakya <nizeshshakya@gmail.com>
2020-06-18 23:26:53 -0500

    Create MaximumSubarray.py

---
+++ src/solutions/dynamicprogramming/MaximumSubarray.py
@@ -0,0 +1,59 @@
+"""
+### GOOGLE INTERVIEW FREQ ASKED ###
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+Solution:
+Approach 2: Greedy
+class Solution:
+    def maxSubArray(self, nums: 'List[int]') -> 'int':
+        n = len(nums)
+        curr_sum = max_sum = nums[0]
+
+        for i in range(1, n):
+            curr_sum = max(nums[i], curr_sum + nums[i])
+            max_sum = max(max_sum, curr_sum)
+            
+        return max_sum
+        
+Complexity Analysis
+Time complexity : O(N) since it's one pass along the array.
+Space complexity : O(1), since it's a constant space solution.
+
+Approach 3: Dynamic Programming (Kadane's algorithm)
+The problem to find sum or maximum or minimum in an entire array or in a fixed-size sliding window could be solved by the dynamic programming (DP) approach in linear time.
+
+There are two standard DP approaches suitable for arrays:
+
+Constant space one. Move along the array and modify the array itself.
+
+Linear space one. First move in the direction left->right, then in the direction right->left. Combine the results. Here is an example.
+
+Let's use here the first approach since one could modify the array to track the current local maximum sum at this given point.
+
+Next step is to update the global maximum sum, knowing the local one.
+
+class Solution:
+    def maxSubArray(self, nums: 'List[int]') -> 'int':
+        n = len(nums)
+        max_sum = nums[0]
+        for i in range(1, n):
+            if nums[i - 1] > 0:
+                nums[i] += nums[i - 1] 
+            max_sum = max(nums[i], max_sum)
+
+        return max_sum
+        
+Complexity Analysis
+Time complexity : O(N) since it's one pass along the array.
+Space complexity : O(1), since it's a constant space solution.
+"""
