45c3867c72929949ea92a1796965c58afaee515e
Nigesh Shakya <nizeshshakya@gmail.com>
2020-06-18 23:18:38 -0500

    Create RobotCleaner.py

---
+++ src/solutions/graph/RobotCleaner.py
@@ -0,0 +1,147 @@
+"""
+### GOOGLE MOST ASKED QUESTION ###
+
+Given a robot cleaner in a room modeled as a grid.
+
+Each cell in the grid can be empty or blocked.
+
+The robot cleaner with 4 given APIs can move forward, turn left or turn right. Each turn it made is 90 degrees.
+
+When it tries to move into a blocked cell, its bumper sensor detects the obstacle and it stays on the current cell.
+
+Design an algorithm to clean the entire room using only the 4 given APIs shown below.
+
+interface Robot {
+  // returns true if next cell is open and robot moves into the cell.
+  // returns false if next cell is obstacle and robot stays on the current cell.
+  boolean move();
+
+  // Robot will stay on the same cell after calling turnLeft/turnRight.
+  // Each turn will be 90 degrees.
+  void turnLeft();
+  void turnRight();
+
+  // Clean the current cell.
+  void clean();
+}
+Example:
+
+Input:
+room = [
+  [1,1,1,1,1,0,1,1],
+  [1,1,1,1,1,0,1,1],
+  [1,0,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0,0],
+  [1,1,1,1,1,1,1,1]
+],
+row = 1,
+col = 3
+
+Explanation:
+All grids in the room are marked by either 0 or 1.
+0 means the cell is blocked, while 1 means the cell is accessible.
+The robot initially starts at the position of row=1, col=3.
+From the top left corner, its position is one row below and three columns right.
+Notes:
+
+The input is only given to initialize the room and the robot's position internally. You must solve this problem "blindfolded". In other words, you must control the robot using only the mentioned 4 APIs, without knowing the room layout and the initial robot's position.
+The robot's initial position will always be in an accessible cell.
+The initial direction of the robot will be facing up.
+All accessible cells are connected, which means the all cells marked as 1 will be accessible by the robot.
+Assume all four edges of the grid are all surrounded by wall.
+
+Solution:
+Approach 1: Spiral Backtracking
+Algorithm
+
+Time to write down the algorithm for the backtrack function backtrack(cell = (0, 0), direction = 0).
+
+Mark the cell as visited and clean it up.
+
+Explore 4 directions : up, right, down, and left (the order is important since the idea is always to turn right) :
+
+Check the next cell in the chosen direction :
+
+If it's not visited yet and there is no obtacles :
+
+Move forward.
+
+Explore next cells backtrack(new_cell, new_direction).
+
+Backtrack, i.e. go back to the previous cell.
+
+Turn right because now there is an obstacle (or a virtual obstacle) just in front.
+
+class Solution(object):       
+    def cleanRoom(self, robot):
+        """
+        :type robot: Robot
+        :rtype: None
+        """
+        def go_back():
+            robot.turnRight()
+            robot.turnRight()
+            robot.move()
+            robot.turnRight()
+            robot.turnRight()
+            
+        def backtrack(cell = (0, 0), d = 0):
+            visited.add(cell)
+            robot.clean()
+            # going clockwise : 0: 'up', 1: 'right', 2: 'down', 3: 'left'
+            for i in range(4):
+                new_d = (d + i) % 4
+                new_cell = (cell[0] + directions[new_d][0], \
+                            cell[1] + directions[new_d][1])
+                
+                if not new_cell in visited and robot.move():
+                    backtrack(new_cell, new_d)
+                    go_back()
+                # turn the robot following chosen direction : clockwise
+                robot.turnRight()
+    
+        # going clockwise : 0: 'up', 1: 'right', 2: 'down', 3: 'left'
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        visited = set()
+        backtrack()
+
+"""
+
+# """
+# This is the robot's control interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Robot:
+#    def move(self):
+#        """
+#        Returns true if the cell in front is open and robot moves into the cell.
+#        Returns false if the cell in front is blocked and robot stays in the current cell.
+#        :rtype bool
+#        """
+#
+#    def turnLeft(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def turnRight(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def clean(self):
+#        """
+#        Clean the current cell.
+#        :rtype void
+#        """
+
+class Solution:
+    def cleanRoom(self, robot):
+        """
+        :type robot: Robot
+        :rtype: None
+        """
